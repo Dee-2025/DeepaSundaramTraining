24a0046d15654461645fdadcb84c8dcdebe311fc
Deepa Shanmuga Sundaram <dssundaram.dfs@dfinsolutions.com>
2025-01-02 16:27:12 +0530

    Second Commit

--- Day17/LabProgram/HelloWorldLoop/HelloWorldLoop/HelloWorldLoop.cs
+++ Day17/LabProgram/HelloWorldLoop/HelloWorldLoop/HelloWorldLoop.cs
@@ -4,7 +4,7 @@
     {
         for(int i=0;i < 10;i++)
         {
-            Console.WriteLine("Hello Premnath");
+            Console.WriteLine("Hello Everone, Welcome to .Net Training");
             
         }
     }
