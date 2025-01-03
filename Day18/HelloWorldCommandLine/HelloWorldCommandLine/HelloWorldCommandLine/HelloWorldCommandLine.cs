97695da40718e4ff380ebf6d1768a7667b2eb826
Deepa Shanmuga Sundaram <dssundaram.dfs@dfinsolutions.com>
2025-01-03 11:47:39 +0530

    Day 18 commandline arguments

---
+++ Day18/HelloWorldCommandLine/HelloWorldCommandLine/HelloWorldCommandLine/HelloWorldCommandLine.cs
@@ -0,0 +1,20 @@
+﻿class HelloWorldCommandLine
+{
+    static void Main(String[] userNames)
+    {
+        if (userNames.Length == 0)
+        {
+            Console.WriteLine("No arguments passed,pls pass argments");
+            return;
+        }
+        foreach (string userName in userNames)
+        {
+            Console.WriteLine("Hello, " + userName + "!");
+        }
+        for (int i = userNames.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(userNames[i].ToUpper());
+        }
+
+    }
+}
