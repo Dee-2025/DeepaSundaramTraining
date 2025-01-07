bac70e8a63142aebc5ca1d2129f381b4bfee228a
Deepa Shanmuga Sundaram <dssundaram.dfs@dfinsolutions.com>
2025-01-08 01:13:11 +0530

    Day 20 work

---
+++ Day20/FunctionOverloading/FunctionOverLoading/FunctionOverLoading/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Program
+{
+
+
+    static void Main()
+    {
+
+        //PrintInfo("Akhilesh");
+        //PrintInfo('@');
+        //PrintInfo(10);
+        //PrintInfo(10.5f);
+        //PrintInfo(10.5d);
+        //PrintInfo(10.5M);
+
+
+
+        PrintInfo(10);
+        PrintInfo(10.55f);
+        PrintInfo(10.55d);
+        PrintInfo(10.55M);
+        PrintInfo('a');
+        PrintInfo('A');
+        PrintInfo("Deepa");
+
+    }
+
+    static void PrintInfo(int input)
+    {
+        Console.WriteLine("Overload int  #1");
+        Console.WriteLine(sizeof(int));
+        Console.WriteLine("input value  is " + input + " and size is " + sizeof(int));
+        Console.WriteLine("input value  is : {0} and size is {1}", input, sizeof(int));
+        Console.WriteLine($"intput is value is {input} and int size is {sizeof(int)}");
+    }
+
+    static void PrintInfo(float input)
+    {
+        Console.WriteLine("Overload float #2");
+        Console.WriteLine($"intput is value is {input} and float size is {sizeof(float)}");
+    }
+
+    static void PrintInfo(double input)
+    {
+        Console.WriteLine("Overload double #3");
+        Console.WriteLine($"intput is value is {input} and double size is {sizeof(double)}");
+    }
+
+    static void PrintInfo(decimal input)
+    {
+        Console.WriteLine("Overload decimal #4");
+        Console.WriteLine($"intput is value is {input} and decimal size is {sizeof(decimal)}");
+    }
+
+    static void PrintInfo(char input)
+    {
+        //ascii value of char
+
+        Console.WriteLine("Overload char #5");
+        Console.WriteLine($"intput is value is {input} and char size is {sizeof(char)} ");
+        Console.WriteLine($"ascii value is {(int)input} ");
+    }
+    static void PrintInfo(string input)
+    {
+        //ascii value of char
+
+        Console.WriteLine("Overload string #5");
+        Console.WriteLine($"intput is value is {input} ");
+        
+    }
+
+
+
+
+}
---
+++ Day20/NameValidater/NameValidator/NameValidator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http.Headers;
+class Program
+{
+    static void Main()
+    {
+       
+        ValidateName();
+    }
+    static void ValidateName()
+    {
+
+        while (true)
+        {
+            Console.WriteLine("Enter your Name");
+            string userName = Console.ReadLine();
+            if (CheckInput(userName))
+            {
+                Console.WriteLine($"Hey {userName} Welcome Back!");
+                break;
+            }
+            Console.WriteLine("Its not Correct Enter Correct Input");
+
+        }
+    }
+    static bool CheckInput(string userName)
+    {
+        
+        foreach (char c in userName)
+        {
+            if ((c == 32))
+                return false;
+            if ((c < 65 || c > 90) && (c < 97 || c > 122))
+                return false;
+        }
+        return true;
+    }
+}
