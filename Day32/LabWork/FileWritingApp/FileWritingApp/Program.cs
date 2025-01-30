cc2b9d40faf64bace02f2f6d1ae06704a7ba3888
Deepa Shanmuga Sundaram <dssundaram.dfs@dfinsolutions.com>
2025-01-30 07:41:13 +0530

    Day 32 Lab Work

---
+++ Day32/LabWork/FileWritingApp/FileWritingApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FileWritingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputFilePath = @"C:\WorkSpace\training\DeepaSundaramTraining\Day32\LabWork\FileWritingApp\output.txt";
+
+            Console.WriteLine("Enter your Name");           
+            File.WriteAllText(outputFilePath, $"User Name: {Console.ReadLine()}");
+            Console.WriteLine("Enter your Address");
+            File.AppendAllText(outputFilePath, $"\nUser Address: {Console.ReadLine()}");
+            
+        }
+    }
+}
---
+++ Day32/LabWork/ReverseArray App/ReverseArray App/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ReverseArray_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "reverse", "this", "array","without","another","array" };
+           
+            ReverseArray(array);
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        static void ReverseArray(string[] array)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left < right)
+            {
+                string temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+}
