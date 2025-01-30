785c4b46351e064a7432709efa65728f330f23d7
Deepa Shanmuga Sundaram <dssundaram.dfs@dfinsolutions.com>
2025-01-30 08:59:28 +0530

    Day 33 Labwork

---
+++ Day33/LabWork/ArrayRotationApp/ArrayRotationApp/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace ArrayRotationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] inputArry = {10, 20, 30, 40, 50, 60, 70};
+            RotateArray(inputArry, 3);
+        }
+        static void RotateArray(int[] arr, int d)
+        {
+            int n = arr.Length;
+            d = d % n; 
+
+            ReverseArray(arr, 0, d - 1); 
+            ReverseArray(arr, d, n - 1); 
+            ReverseArray(arr, 0, n - 1); 
+            foreach (int i in arr) {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void ReverseArray(int[] arr, int start, int end)
+        {
+            while (start < end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
---
+++ Day33/LabWork/CustomException/AccountCommonLib/Models/InsufiiciantFundsException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountCommonLib.Models
+{
+    public class InsufficientFundsException : Exception
+    {
+         private Account _accountHolder;//HasA relationship
+        public InsufficientFundsException(Account account)
+        {
+
+            _accountHolder = account;
+        }
+        public override string Message
+        {
+            get
+            {
+
+                return $"account holder:{_accountHolder.Name} , doesnot have sufficends for withraw of balacne is only ${_accountHolder.Balance}";
+            }
+        }
+
+
+
+
+    }
+
+
+}
