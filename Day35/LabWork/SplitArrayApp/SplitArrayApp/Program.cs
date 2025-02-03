ebf1d2e0fea84e561ca9f0527c5d73b8369a72f8
Deepa Shanmuga Sundaram <dssundaram.dfs@dfinsolutions.com>
2025-02-03 08:41:30 +0530

    Day 35 Lab Work

---
+++ Day35/LabWork/SplitArrayApp/SplitArrayApp/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SplitArrayApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = new string[] { "google.com", "tneb.org", "rrd.com", "amazon.in", "sc.com"};
+            SplitArray(inputArray);
+        }
+
+        static void SplitArray(string[] inputArray)
+        {
+            string[] outputArray = new string[inputArray.Length];
+            foreach (string item in inputArray)
+            {
+                bool isDotPresent = false;
+                string beforeDot = string.Empty;
+                
+                foreach (char c in item)
+                {
+                    if (c == '.')
+                    {
+                        isDotPresent = true;
+                        continue; 
+                    }
+                    if (!isDotPresent)                   
+                          
+                        beforeDot += c.ToString();                   
+                }
+                outputArray[Array.IndexOf(inputArray, item)] = beforeDot;
+            }
+            foreach (string item in outputArray)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
