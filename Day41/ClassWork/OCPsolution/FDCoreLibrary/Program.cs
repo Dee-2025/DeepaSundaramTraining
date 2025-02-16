9f71f3b9e0789fe1ea7304751e5cd5c6263f0ef3
Deepa Shanmuga Sundaram <dssundaram.dfs@dfinsolutions.com>
2025-02-16 21:02:43 +0530

    Day 39 to 44

---
+++ Day41/ClassWork/OCPsolution/FDCoreLibrary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FDCoreLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Day42/ClassWork/ProductDatabaseConnectionApp/ProductDatabaseConnectionApp/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.Data.SqlClient;
+
+namespace ProductDatabaseConnectionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CaseStudy1();
+            CaseStudy2();
+        }
+
+        private static void CaseStudy2()
+        {
+            var connectionString = "server=.\\sqlexpress;database=rrd_db1;Integrated security=true;TrustServerCertificate=true";
+            using (var con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (var cmd = new SqlCommand("select * from product", con))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["name"]);
+                            Console.WriteLine(reader["quantity"]);
+                            Console.WriteLine(reader["price"]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void CaseStudy1()
+        {
+            var connectionString = "server=.\\sqlexpress;database=rrd_db1;Integrated security=true;TrustServerCertificate=true";
+            using (var con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                Console.WriteLine("connection opened connected to ");
+                Console.WriteLine(con.Database);
+                Console.WriteLine("connection state is ");
+                Console.WriteLine(con.State);
+            }
+        }
+    }
+}
